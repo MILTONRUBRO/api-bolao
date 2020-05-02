57d0ff1fffa4f6e1f56df7311e00d6f7addf0e16
Milton Oliveira Souza <miltonrubro@gmail.com>
2020-05-02 17:02:02 -0300

    Criação do setup do projeto com springboot

---
+++ src/main/java/br/com/devmos/apibolao/ApiBolaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.devmos.apibolao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBolaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiBolaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/devmos/apibolao/ApiBolaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.devmos.apibolao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBolaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
